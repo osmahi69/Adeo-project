47eac875f92886c4ac45a299b741c110580f0ca2
Oussama <smahioussama1@gmail.com>
2020-12-13 16:18:52 +0100

    create store and design home page

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,45 @@
-// eslint-disable
 <template>
   <div id="app">
     <v-app class="app">
       <v-app-bar color="#F0F8FF">
-        <template> </template>
         <v-avatar @click="goToHome()" class="avatar">
-          <!-- <v-img
-            :src="require('./assurup.jpg')"
-            class="grey lighten-2"
+          <v-img
+            :src="require('./assets/Adeo_logo.jpg')"
+            class="grey lighten-2 pl-6"
             width="0"
-          /> -->
+          />
         </v-avatar>
         <v-toolbar-title color="blue" class="ml-3 headerTitle"
-          >Assurup</v-toolbar-title
+          >HALLEY</v-toolbar-title
         >
         <template v-slot:extension>
           <v-tabs align-with-title>
-            <v-tab @click="goToHome()">Home</v-tab>
-            <v-tab @click="goToProducts()">Products</v-tab>
+            <v-tab @click="goToHome()">HOME</v-tab>
+            <v-tab @click="goToStore()">STORE</v-tab>
           </v-tabs>
         </template>
       </v-app-bar>
-      <v-container style="height: 1000px">
+      <v-container style="height: 1000px" class="container">
         <router-view></router-view>
       </v-container>
     </v-app>
   </div>
 </template>
 <script>
+import stores from '@/assets/data/stores.json'
+
 export default {
   name: 'App',
   methods: {
     goToHome () {
       this.$router.push('/')
     },
-    goToProducts () {
-      this.$router.push('/products')
+    goToStore () {
+      this.$router.push('/store')
     }
+  },
+  mounted () {
+    this.$store.dispatch('stores/setValue', { list: stores })
   }
 }
 </script>
@@ -46,5 +49,7 @@ export default {
 }
 .headerTitle {
   color: blue;
+  font-family: Helvetica;
+  text-shadow: 0px 0px 5px red;
 }
 </style>
